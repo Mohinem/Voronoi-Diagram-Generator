129210da71cc9b5c16787469ec4783d9ebd95675
mohinem <rickyigl10@gmail.com>
2020-12-25 02:48:03 +0530

    Add quit event handling

--- voronoi.py
+++ voronoi.py
@@ -3,4 +3,9 @@ import pygame
 display = pygame.display.set_mode((800,600))
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
     pygame.display.update()
