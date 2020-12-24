5affc3d9f1d1fc5f208a6489bdd4f6af99ab3b57
mohinem <rickyigl10@gmail.com>
2020-12-25 04:12:29 +0530

    Add left and right click for creating voronoi diagram

--- voronoi.py
+++ voronoi.py
@@ -5,19 +5,45 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 COLOR_MIN_VALUE = 0
 COLOR_MAX_VALUE = 255
+MASSIVE_VALUE = 9999999999999999999999999999999999999999999999999
+LEFT = 1
+RIGHT = 3
 
-display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+gameDisplay = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 list_of_positions = []
 random.seed()
+
+
+def recolor():
+    pixelarray = pygame.PixelArray(gameDisplay)
+
+    for i in range(0, SCREEN_WIDTH):
+        for j in range(0, SCREEN_HEIGHT):
+            min_distance = MASSIVE_VALUE
+            index = 0
+            for k in range (0,len(list_of_positions)):
+                temp_x, temp_y, temp_color = list_of_positions[k]
+                if min_distance > ((temp_x - i) * (temp_x - i) + (temp_y - j) * (temp_y - j)):
+                    min_distance = ((temp_x - i) * (temp_x - i) + (temp_y - j) * (temp_y - j))
+                    index = k
+
+            temp_x, temp_y, temp_color = list_of_positions[index]
+            pixelarray[i][j] = temp_color
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
             x, y = event.pos
             color = (random.randint(COLOR_MIN_VALUE, COLOR_MAX_VALUE), random.randint(COLOR_MIN_VALUE, COLOR_MAX_VALUE), random.randint(COLOR_MIN_VALUE, COLOR_MAX_VALUE))
             list_of_positions.append((x, y, color))
+            recolor()
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT:
+            list_of_positions.pop()
+            recolor()
 
     pygame.display.update()
